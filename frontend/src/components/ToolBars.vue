44e2cb897a1ced59f8be9333e6ee7d444230d1e3
GlomGlom <apichate.yatra@gmail.com>
2020-01-04 22:30:51 +0700

    close #44 - แก้ไข UI ของระบบย่อย ระบบขอใช้สิทธิ์กรมธรรม์

--- frontend/src/components/ToolBars.vue
+++ frontend/src/components/ToolBars.vue
@@ -1,55 +1,84 @@
 <template>
   <div>
-    <v-toolbar extended extension-height="25%" flat color="#D31145">
+    <v-toolbar flat extended extension-height="20%" color="#D31145">
+      <v-app-bar-nav-icon
+        class="hidden-md-and-up mt-6 white--text"
+        @click.stop=" drawer = !drawer "
+      ></v-app-bar-nav-icon>
       <v-toolbar-title class="white--text display-1 mt-6">
         <div class="font-style">
-          <span>SUTCARE</span>
+          <router-link to="/Home" tag="span" style="cursor: pointer">SUT-CARE</router-link>
         </div>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <div class="font-buttom">
-        <div class="mt-6">
-          <v-row align="center">
-            <v-col cols="12" sm="3">
-              <v-btn x-large text class="white--text">
-                <span>หน้าแรก</span>
-              </v-btn>
-            </v-col>
-            <v-col cols="12" sm="3">
-              <v-btn x-large text class="white--text">
-                <span>สมัครกรมธรรม์</span>
-              </v-btn>
-            </v-col>
-            <v-col cols="12" sm="3">
-              <v-btn x-large text class="white--text">
-                <span>ขอใช้สิทธิ์กรมธรรม์</span>
-              </v-btn>
-            </v-col>
-            <v-col cols="12" sm="3">
-              <v-btn x-large text class="white--text">
-                <span>ออกจากระบบ</span>
-                <v-icon right>exit_to_app</v-icon>
-              </v-btn>
-            </v-col>
-          </v-row>
+      <v-toolbar-items class="hidden-xs-only hidden-sm-only">
+        <div class="font-buttom">
+          <div class="mt-4">
+            <v-btn
+              x-large
+              text
+              class="white--text"
+              v-for="item in menuItems"
+              :key="item.title"
+              :to="item.link"
+            >
+              <v-icon left dark>{{ item.icon }}</v-icon>
+              {{ item.title }}
+            </v-btn>
+          </div>
         </div>
-      </div>
+      </v-toolbar-items>
     </v-toolbar>
+
+    <v-navigation-drawer temporary v-model="drawer" app>
+      <v-list>
+        <div class="font-buttom">
+          <v-list-item v-for="item in menuItems" :key="item.title" :to="item.link">
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-tile-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-tile-content>
+          </v-list-item>
+        </div>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      drawer: false,
+      menuItems: [
+        { icon: "home", title: "หน้าแรก", link: "/Home" },
+        {
+          icon: "how_to_reg",
+          title: "สมัครกรมธรรม์",
+          link: "/RegisterInsurance"
+        },
+        {
+          icon: "assignment_turned_in",
+          title: "ขอใช้สิทธิ์กรมธรรม์",
+          link: "/PrivilegeInsurance"
+        },
+        { icon: "exit_to_app", title: "ออกจากระบบ", link: "/UserLogin" }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-@import url("https://fonts.googleapis.com/css?family=Fredoka+One|Kanit&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Itim|Sriracha&display=swap");
 
 .font-style {
-  font-family: "Fredoka One", cursive;
+  font-family: "Sriracha", cursive;
 }
 .font-buttom {
-  font-family: "Kanit", sans-serif;
-  padding: 0.5%;
+  font-family: "Itim", cursive;
 }
 </style>
