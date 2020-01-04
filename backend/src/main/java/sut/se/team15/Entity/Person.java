af1445d748ebeef4c6beb745c342180c5c0934ae
KanokpornD <mayka2727@gmail.com>
2020-01-05 00:02:01 +0700

    close #46 - แก้ไข Entity ของระบบย่อย ระบบสมัครกรมธรรม์

--- backend/src/main/java/sut/se/team15/Entity/Person.java
+++ backend/src/main/java/sut/se/team15/Entity/Person.java
@@ -3,18 +3,14 @@ package sut.se.team15.Entity;
 import lombok.*;
 
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
+
 
 @Data
 @Entity
@@ -30,5 +26,7 @@ public class Person {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Person_seq")
 	@Column(name = "Person_ID", unique = true, nullable = true)
     private @NonNull Long PersonId;
-    
+	private @NonNull String Persontype;
+	
+	
 }
