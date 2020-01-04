e8aa4cc82ea237f30747d4e278f1eaa6e20d892b
GlomGlom <apichate.yatra@gmail.com>
2020-01-04 20:12:55 +0700

    close #39 - ทำ Router UI ของระบบประกันสุขภาพ

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,66 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import MainLogin from '../components/MainLogin.vue' 
+import UserLogin from '../components/UserLogin.vue'
+
+import CreateInsurance from '../views/CreateInsurance.vue'
+import Hospital from '../views/Hospital.vue'
+import PayInsurance from '../views/PayInsurance.vue'
+import Register from '../views/Register.vue'
+
+import Home from '../views/Home.vue'
+import RegisterInsurance from '../views/RegisterInsurance.vue'
+import PrivilegeInsurance from '../views/PrivilegeInsurance.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
- 
+  {
+    path: '/',
+    name: 'MainLogin',
+    component: MainLogin
+  },
+  {
+    path: '/UserLogin',
+    name: 'UserLogin',
+    component: UserLogin
+  },
+  {
+    path: '/CreateInsurance',
+    name: 'CreateInsurance',
+    component: CreateInsurance
+  },
+  {
+    path: '/Hospital',
+    name: 'Hospital',
+    component: Hospital
+  },
+  {
+    path: '/PayInsurance',
+    name: 'PayInsurance',
+    component: PayInsurance
+  },
+  {
+    path: '/Register',
+    name: 'Register',
+    component: Register
+  }, 
+  {
+    path: '/Home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/RegisterInsurance',
+    name: 'RegisterInsurance',
+    component: RegisterInsurance
+  }, 
+  {
+    path: '/PrivilegeInsurance',
+    name: 'PrivilegeInsurance',
+    component: PrivilegeInsurance
+  },
   
 ]
 
