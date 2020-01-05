0800e2b461c2d95a8c51ff074ac17512e2c23212
Arm_Nopparat <42844201+noparat41@users.noreply.github.com>
2020-01-05 20:10:55 +0700

    close #63 - ทำ API ของระบบย่อย ระบบลงทะเบียนโรงพยาบาลในเครือประกันสุขภาพ

--- frontend/src/views/Hospital.vue
+++ frontend/src/views/Hospital.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="inspire">
     <v-app-bar app clipped-left>
-      <v-icon size="30" >apartment</v-icon>
+      <v-icon size="30">apartment</v-icon>
       <v-toolbar-title>Register Hospital</v-toolbar-title>
       <v-spacer />
       <v-menu bottom left>
@@ -37,22 +37,46 @@
               <v-col>
                 <v-row>
                   <v-col class="d-flex" cols="12" sm="8">
-                    <v-text-field label="Hospital Name"></v-text-field>
+                    <v-text-field label="Hospital Name" v-model="HospitalName"></v-text-field>
                   </v-col>
                   <v-col class="d-flex" cols="12" sm="4">
-                    <v-select :items="items" label="Hospital Type" outlined></v-select>
+                    <v-select
+                      :items="Type"
+                      label="Hospital Type"
+                      v-model="Hospital.TypeID"
+                      item-text="typeName"
+                      item-value="id"
+                      :rules="[(v) => !!v || 'Item is required']"
+                      outlined
+                    ></v-select>
                   </v-col>
                 </v-row>
-                <v-text-field color="white" label="Location Details"></v-text-field>
+                <v-text-field color="white" label="Location Details" v-model="LocationDetails"></v-text-field>
                 <v-row>
                   <v-col class="d-flex" cols="12" sm="4">
-                    <v-select :items="items" label="Region" outlined></v-select>
+                    <v-select
+                      :items="Region"
+                      label="Region"
+                      v-model="Hospital.RegionID"
+                      item-text="regionName"
+                      item-value="id"
+                      :rules="[(v) => !!v || 'Item is required']"
+                      outlined
+                    ></v-select>
                   </v-col>
                   <v-col class="d-flex" cols="12" sm="8">
-                    <v-select :items="items" label="Province" outlined></v-select>
+                    <v-select
+                      :items="Province"
+                      label="Province"
+                      v-model="Hospital.ProvinceID"
+                      item-text="provinceName"
+                      item-value="id"
+                      :rules="[(v) => !!v || 'Item is required']"
+                      outlined
+                    ></v-select>
                   </v-col>
                 </v-row>
-                <v-text-field color="white" label="Telephone number"></v-text-field>
+                <v-text-field color="white" label="Telephone number" v-model="TelephoneNumber"></v-text-field>
               </v-col>
             </v-col>
           </v-card-text>
@@ -61,7 +85,7 @@
 
           <v-card-actions>
             <v-col>
-              <v-btn rounded large color="success" @click="$router.push('/')">Save</v-btn>
+              <v-btn rounded large color="success" @click="save">Save</v-btn>
               <v-btn color="#ffffff" @click="$router.push('/')" text large>cancel</v-btn>
             </v-col>
           </v-card-actions>
@@ -77,14 +101,95 @@
 </template>
 
 <script>
+import http from "../api/http-common";
 export default {
   data() {
     return {
       items: [
         { icon: "home", text: "Home", route: "/Main" },
         { icon: "exit_to_app", text: "Sign Out", route: "/" }
-      ]
+      ],
+      Hospital: {
+        TypeID: "",
+        ProvinceID: "",
+        RegionID: ""
+      },
+      Type: [],
+      Province: [],
+      Region: [],
+      HospitalName: "",
+      LocationDetails: "",
+      TelephoneNumber: ""
     };
+  },
+  methods: {
+    /* eslint-disable no-console */
+    getType() {
+      console.log("HospitalType");
+      http
+        .get("HospitalType")
+        .then(response => {
+          this.Type = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    getProvince() {
+      console.log("Province");
+      http
+        .get("/Province")
+        .then(response => {
+          this.Province = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    getRegion() {
+      console.log("Region");
+      http
+        .get("/Region")
+        .then(response => {
+          this.Region = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    save() {
+      console.log("Save");
+      http
+        .post(
+          "/AddHospital/" +
+            this.Hospital.TypeID +
+            "/" +
+            this.Hospital.RegionID +
+            "/" +
+            this.Hospital.ProvinceID +
+            "/" +
+            this.HospitalName +
+            "/" +
+            this.LocationDetails +
+            "/" +
+            this.TelephoneNumber,
+          this.Hospital
+        )
+        .then(responses => {
+          console.log(responses);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+  mounted() {
+    this.getType();
+    this.getProvince();
+    this.getRegion();
   },
 
   created() {
