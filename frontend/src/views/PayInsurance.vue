64211782b0b0c120c8645df9ab607d8573ee57b7
wittawat 1998 <eatrnny@gmail.com>
2020-01-05 23:35:52 +0700

    close #67 - ทำ API ของระบบย่อย ระบบจ่ายเบี้ยกรมธรรม์

--- frontend/src/views/PayInsurance.vue
+++ frontend/src/views/PayInsurance.vue
@@ -37,8 +37,10 @@
             <v-row>
             <v-col class="d-flex" cols="13" sm="8">
               <v-select
+                :items="Register"
                 label="รายชื่อสมาชิกประกันสุขภาพ"
-                item-text="typename"
+                v-model="PayInsurance.RegID"
+                item-text="fristName"
                 item-value="id"
                 :rules="[(v) => !!v || 'Item is required']"
               ></v-select>
@@ -50,35 +52,43 @@
           <v-row>
             <v-col class="d-flex" cols="12" sm="6">
               <v-select
+              :items="Insurance"
                 label="กรมธรรม์"
-                item-text="categoryname"
-                item-value="id"
+                v-model="PayInsurance.InsuranceID"
+                item-text="createInsurance_name"
+                item-value="createInsurance_id"
                 :rules="[(v) => !!v || 'Item is required']"
               ></v-select>
             </v-col>
 
             <v-col cols="5" sm="2" md="5">
-              <v-text-field label="จำนวนเงิน" :rules="[(v) => !!v || 'Item is required']"></v-text-field>
+              <v-text-field label="จำนวนเงิน" 
+              v-model="Amount"
+              :rules="[(v) => !!v || 'Item is required']"></v-text-field>
             </v-col>
           </v-row>
-        </v-container>
+       
         <v-row>
             <v-col class="d-flex" cols="12" sm="6">
               <v-select
-                label="พนักงานประสุขภาพ"
-                item-text="categoryname"
-                item-value="id"
-                :rules="[(v) => !!v || 'Item is required']"
+                      :items="Staff"
+                      label="พนักงานประกันสุขภาพ"
+                      v-model="PayInsurance.StaffID"
+                      item-text="staff_name"
+                      item-value="staff_id"
+                      :rules="[(v) => !!v || 'Item is required']"
+                      
               ></v-select>
             </v-col>
           
           </v-row>
+           </v-container>
           <v-divider></v-divider>
 
           <v-card-actions>
             <v-col>
-               <v-btn rounded large color="#D31145" @click="$router.push('/')">Save</v-btn>
-                <v-btn  @click="$router.push('/')" text large>cancel</v-btn>
+               <v-btn rounded large color="#D31145" @click="savePay">Save</v-btn>
+                <v-btn  @click="cancel" text large>cancel</v-btn>
             </v-col>
           </v-card-actions>
         </v-card>
@@ -92,14 +102,99 @@
 </template>
 
 <script>
+import http from "../api/http-common";
 export default {
   data() {
     return {
       items: [
         { icon: "home", text: "Home", route: "/Main" },
         { icon: "exit_to_app", text: "Sign Out", route: "/" }
-      ]
+      ],
+      PayInsurance: {
+       RegID: "",
+        StaffID: "",
+        InsuranceID: ""
+      },
+      Insurance: [],
+      Register: [],
+      Staff: [],
+      Amount: ""
     };
+  },
+  methods: {
+    /* eslint-disable no-console */
+       getRegister() {
+      console.log("register");
+      http
+        .get("/Register")
+        .then(response => {
+          this.Register = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+      getStaff() {
+      console.log("staff");
+      http
+        .get("/staff")
+        .then(response => {
+          this.Staff = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+     getInsurance() {
+      console.log("insurance");
+      http
+        .get("/CreateInsurance")
+        .then(response => {
+          this.Insurance = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    savePay() {
+      console.log("Save");
+      console.log(this.PayInsurance.RegID);
+      console.log(this.PayInsurance.InsuranceID);
+      console.log(this.Amount);
+      console.log(this.PayInsurance.StaffID);
+      
+      http
+        .post(
+          "/payinsurances/" +
+            this.PayInsurance.RegID +
+            "/" +
+            this.PayInsurance.InsuranceID +
+            "/" +
+            this.Amount+
+            "/" +
+           this.PayInsurance.StaffID,
+           this.PayInsurance
+        )
+        .then(responses => {
+          console.log(responses);
+          window.location.reload();
+          alert("successfully");
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    },
+    cancel(){
+      window.location.reload();
+    }
+  },
+  mounted() {
+    this.getStaff();
+    this.getRegister();
+    this.getInsurance();
   },
 
   created() {
