117cc1ff6e3e82aa6296732e4766a36cd9477bee
GlomGlom <apichate.yatra@gmail.com>
2020-01-08 23:02:59 +0700

    close #65 - แก้ไข entity ของระบบย่อย ระบบขอใช้สิทธิ์กรมธรรม์

---
+++ backend/src/main/java/sut/se/team15/Entity/PurposeRequest.java
@@ -0,0 +1,33 @@
+package sut.se.team15.Entity;
+
+import lombok.*;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+
+@Data
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Table(name = "Purpose_Request")
+public class PurposeRequest {
+    @Id
+    @SequenceGenerator(name = "Purpose_Request_SEQ", sequenceName = "Purpose_Request_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "Purpose_Request_SEQ")
+    @Column(name = "Purpose_Request_ID", unique = true, nullable = true)
+
+    private @NonNull Long id;
+    private @NonNull String purposeRequest;
+}
