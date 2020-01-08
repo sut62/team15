0fabca1a2c94fe19ef57ea79942a5cd652f14c20
wittawat 1998 <eatrnny@gmail.com>
2020-01-08 23:27:36 +0700

    close #85 - ทำหน้าUI ของ Admin

---
+++ frontend/src/components/HomeAdmin.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <v-toolbar flat height="100px" tile color="deep-purple">
+      <div height="110px">
+      <v-img  dark src="../assets/Test1.png"></v-img></div>
+      <v-spacer></v-spacer>
+      <v-menu bottom left>
+        <template v-slot:activator="{ on }">
+          <v-btn dark icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-card height="mx-auto" width="200" dark>
+          <v-list>
+            <v-list-item @click="$router.push('/HomeAdmin')">
+              <v-col class="8">
+                <v-icon>person</v-icon>
+              </v-col>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$router.push('/Hospital')">
+              <v-col class="8">
+                <v-icon>person</v-icon>
+              </v-col>
+              <v-list-item-title>Hospital</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$router.push('/CreateInsurance')">
+              <v-col class="8">
+                <v-icon>person</v-icon>
+              </v-col>
+              <v-list-item-title>CreateInsurance</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$router.push('/PayInsurance')">
+              <v-col class="8">
+                <v-icon>person</v-icon>
+              </v-col>
+              <v-list-item-title>PayInsurance</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$router.push('/')">
+              <v-col class="8">
+                <v-icon>exit_to_app</v-icon>
+              </v-col>
+              <v-list-item-title>Sign Out</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
+    </v-toolbar>
+
+    <v-carousel height="600" whight="1165" tile cycle hide-delimiter-background show-arrows-on-hover>
+      <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: require("../assets/Slide-1.jpg")
+        },
+        {
+          src: require("../assets/Slide-2.jpg")
+        },
+        {
+          src: require("../assets/Slide-3.jpg")
+        }
+      ]
+    };
+  }
+};
+</script>
---
+++ frontend/src/components/SingInAdmin.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <div
+        class="img-background"
+        v-bind:style="{'background-image': 'url(' + require('../assets/AdminOne.jpg') + ')'}"
+      >
+        <v-container class="fill-height" fluid>
+          <v-row align="center" justify="center">
+            <v-col md="8">
+              <v-card class="elevation-12">
+                <v-row class="mx-auto">
+                  <v-img height="mx-auto" width="325" dark src="../assets/Test2.png"></v-img>
+                  <v-col class="text-center" md="mx-auto">
+                    <v-container class="mx-auto">
+                      <v-col cols="12" sm="3"></v-col>
+                      <v-row class="justify-center">
+                        <v-icon size="50" color="#F4511E">account_circle</v-icon>
+                        <v-card-text>
+                          <h1>
+                            <span>Sign in</span>
+                          </h1>
+                          <v-form>
+                            <v-text-field
+                              label="๊email"
+                              name="email"
+                              v-model="email"
+                              prepend-icon="person"
+                              type="email"
+                            ></v-text-field>
+
+                            <v-text-field
+                              id="password"
+                              label="Password"
+                              name="password"
+                              v-model="password"
+                              prepend-icon="lock"
+                              type="password"
+                            ></v-text-field>
+                          </v-form>
+
+                          <v-row class="justify-center">
+                            <v-btn
+                              rounded
+                              color="#F4511E"
+                              dark
+                              large
+                              @click="$router.push('/HomeAdmin')"
+                            >login</v-btn>
+                            <v-btn
+                              rounded
+                              text
+                              color="#F4511E"
+                              @click="$router.push('/')"
+                              outlined
+                              large
+                            >cancel</v-btn>
+                          </v-row>
+                        </v-card-text>
+                      </v-row>
+                    </v-container>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import http from "../api/http-common";
+export default {
+  name: "Signin",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      Staff: []
+    };
+  },
+
+  methods: {
+    /* eslint-disable no-console */
+    getStaff() {
+      console.log("staff");
+      http
+        .get("/staff")
+        .then(response => {
+          this.Staff = response.data;
+          console.log(response.data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+    }
+  },
+
+  mounted() {
+    this.getStaff();
+  }
+};
+</script>
+
