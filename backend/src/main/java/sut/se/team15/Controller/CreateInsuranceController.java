48b40671f04efac64369f96110cec229ef57ef79
RakSR <sirilukrongkratok@gmail.com>
2020-01-22 11:56:18 +0700

    close #130 -แก้ไข Controller ของระบบย่อย ระบบจัดโปรโมชั่น

--- backend/src/main/java/sut/se/team15/Controller/CreateInsuranceController.java
+++ backend/src/main/java/sut/se/team15/Controller/CreateInsuranceController.java
@@ -37,20 +37,21 @@ public class CreateInsuranceController {
         return createInsuranceRepository.findAll().stream().collect(Collectors.toList());
     }
 
-    @PostMapping("/CreateInsurance/{Name}/{AccidentCoverage}/{DiseaseCoverage}/{ProtectionRights}/{TermOfProtection}/{InsurancePremium}/{InsuranceType_id}/{PaymentPeriod_id}/{ContractDueDate_id}")
+    @PostMapping("/CreateInsurance/{Name}/{AccidentCoverage}/{DiseaseCoverage}/{ProtectionRights}/{TermOfProtection}/{InsurancePremium}/{InsuranceType_id}/{ContractDueDate_id}/{PaymentPeriod_id}")
     public CreateInsurance newCreateInsurance(CreateInsurance newCreateInsurance, @PathVariable String Name,
             @PathVariable String AccidentCoverage, @PathVariable String DiseaseCoverage,
             @PathVariable String ProtectionRights, @PathVariable String TermOfProtection,
-            @PathVariable Double InsurancePremium, @PathVariable long InsuranceType_id, @PathVariable long PaymentPeriod_id,
-            @PathVariable long ContractDueDate_id) {
+            @PathVariable Double InsurancePremium, @PathVariable long InsuranceType_id,
+            @PathVariable long ContractDueDate_id,@PathVariable long PaymentPeriod_id) {
 
         InsuranceType insuranceTypeID = insuranceTypeRepository.findById(InsuranceType_id);
-        PaymentPeriod paymentPeriodID = paymentPeriodRepository.findById(PaymentPeriod_id);
         ContractDueDate contractDueDateID = contractDueDateRepository.findById(ContractDueDate_id);
+        PaymentPeriod paymentPeriodID = paymentPeriodRepository.findById(PaymentPeriod_id);
+        
 
         newCreateInsurance.setInsuranceTypeId(insuranceTypeID);
-        newCreateInsurance.setPaymentPeriodId(paymentPeriodID);
         newCreateInsurance.setContractDueDateId(contractDueDateID);
+        newCreateInsurance.setPaymentPeriodId(paymentPeriodID);
 
         newCreateInsurance.setCreateInsurance_name(Name);
         newCreateInsurance.setCreateInsurance_accidentCoverage(AccidentCoverage);
