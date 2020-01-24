f21b8d251f290e4e7521fcbbb2c5749c5c2e57b0
Arm_Nopparat <42844201+noparat41@users.noreply.github.com>
2020-01-25 06:07:16 +0700

    close #95 - ทำ UI ของระบบย่อย ระบบค้นหาโรงพยาบาลในเครือประกันสุขภาพ

--- frontend/src/views/Hospital.vue
+++ frontend/src/views/Hospital.vue
@@ -12,7 +12,7 @@
           </v-btn>
         </template>
 
-        <v-card height="mx-auto" width="200" dark>
+        <v-card height="mx-auto" width="250" dark>
           <v-list>
             <v-list-item @click="$router.push('/HomeAdmin')">
               <v-col class="8">
@@ -25,6 +25,12 @@
                 <v-icon>apartment</v-icon>
               </v-col>
               <v-list-item-title>Hospital</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="$router.push('/HospitalSearch')">
+              <v-col class="8">
+                <v-icon>search</v-icon>
+              </v-col>
+              <v-list-item-title>HospitalSearch</v-list-item-title>
             </v-list-item>
             <v-list-item @click="$router.push('/CreateInsurance')">
               <v-col class="8">
@@ -87,7 +93,43 @@
                         outlined
                       ></v-select>
                     </v-col>
-                    <v-col class="d-flex" cols="12" sm="8">
+                    <v-col class="d-flex" cols="12" sm="8" v-if="this.Hospital.RegionID == 1">
+                      <v-select
+                        :items="Province1"
+                        label="Province"
+                        v-model="Hospital.ProvinceID"
+                        :rules="[(v) => !!v || 'Item is required']"
+                        outlined
+                      ></v-select>
+                    </v-col>
+                    <v-col class="d-flex" cols="12" sm="8" v-else-if="this.Hospital.RegionID == 2">
+                      <v-select
+                        :items="Province2"
+                        label="Province"
+                        v-model="Hospital.ProvinceID"
+                        :rules="[(v) => !!v || 'Item is required']"
+                        outlined
+                      ></v-select>
+                    </v-col>
+                    <v-col class="d-flex" cols="12" sm="8" v-else-if="this.Hospital.RegionID == 3">
+                      <v-select
+                        :items="Province3"
+                        label="Province"
+                        v-model="Hospital.ProvinceID"
+                        :rules="[(v) => !!v || 'Item is required']"
+                        outlined
+                      ></v-select>
+                    </v-col>
+                    <v-col class="d-flex" cols="12" sm="8" v-else-if="this.Hospital.RegionID == 4">
+                      <v-select
+                        :items="Province4"
+                        label="Province"
+                        v-model="Hospital.ProvinceID"
+                        :rules="[(v) => !!v || 'Item is required']"
+                        outlined
+                      ></v-select>
+                    </v-col>
+                    <v-col class="d-flex" cols="12" sm="8" v-else>
                       <v-select
                         :items="Province"
                         label="Province"
@@ -109,7 +151,7 @@
             <v-card-actions>
               <v-col>
                 <v-btn rounded large color="success" @click="save">Save</v-btn>
-                <v-btn color="#ffffff" @click="$router.push('/')" text large>cancel</v-btn>
+                <v-btn color="#ffffff" @click="cancel()" text large>cancel</v-btn>
               </v-col>
             </v-card-actions>
           </v-card>
@@ -117,9 +159,9 @@
 
         <v-dialog v-model="dialog" max-width="290">
           <v-card>
-            <br/>
-            <v-card-text>Successfully</v-card-text>
-
+            <br />
+            <v-card-text v-if="status==true">Successfully</v-card-text>
+            <v-card-text v-else>Please fill out your information correctly and completely.</v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="green darken-1" text @click="dialog = false">close</v-btn>
@@ -141,6 +183,7 @@ import http from "../api/http-common";
 export default {
   data() {
     return {
+      status: false,
       dialog: false,
       Hospital: {
         TypeID: "",
@@ -148,11 +191,24 @@ export default {
         RegionID: ""
       },
       Type: [],
-      Province: [],
       Region: [],
       HospitalName: "",
       LocationDetails: "",
-      TelephoneNumber: ""
+      TelephoneNumber: "",
+      Province: [],
+      Province1: [],
+      Province2: [],
+      Province3: [],
+      Province4: [],
+      ProvinceId1: [],
+      ProvinceId2: [],
+      ProvinceId3: [],
+      ProvinceId4: [],
+      n1: 0,
+      n2: 0,
+      n3: 0,
+      n4: 0,
+      i: 0
     };
   },
   methods: {
@@ -176,6 +232,7 @@ export default {
         .then(response => {
           this.Province = response.data;
           console.log(response.data);
+          this.separate();
         })
         .catch(e => {
           console.log(e);
@@ -193,8 +250,77 @@ export default {
           console.log(e);
         });
     },
+    separate() {
+      for (this.i = 0; this.i < 77; this.i++) {
+        if (this.Province[this.i].regionId.id == 1) {
+          this.Province1[this.n1] = this.Province[this.i].provinceName;
+          this.ProvinceId1[this.n1++] = this.Province[this.i].id;
+        } else if (this.Province[this.i].regionId.id == 2) {
+          this.Province2[this.n2] = this.Province[this.i].provinceName;
+          this.ProvinceId2[this.n2++] = this.Province[this.i].id;
+        } else if (this.Province[this.i].regionId.id == 3) {
+          this.Province3[this.n3] = this.Province[this.i].provinceName;
+          this.ProvinceId3[this.n3++] = this.Province[this.i].id;
+        } else {
+          this.Province4[this.n4] = this.Province[this.i].provinceName;
+          this.ProvinceId4[this.n4++] = this.Province[this.i].id;
+        }
+      }
+      console.log(this.Province1);
+      console.log(this.Province2);
+      console.log(this.Province3);
+      console.log(this.Province4);
+    },
+    convert() {
+      if (this.Hospital.RegionID == 1) {
+        for (this.i = 0; this.i < this.n1; this.i++) {
+          if (this.Province1[this.i] == this.Hospital.ProvinceID) {
+            this.Hospital.ProvinceID = this.ProvinceId1[this.i]-1;
+            break;
+          }
+        }
+      }
+      if (this.Hospital.RegionID == 2) {
+        for (this.i = 0; this.i < this.n2; this.i++) {
+          if (this.Province2[this.i] == this.Hospital.ProvinceID) {
+            this.Hospital.ProvinceID = this.ProvinceId2[this.i]-1;
+            break;
+          }
+        }
+      }
+      if (this.Hospital.RegionID == 3) {
+        for (this.i = 0; this.i < this.n3; this.i++) {
+          if (this.Province3[this.i] == this.Hospital.ProvinceID) {
+            this.Hospital.ProvinceID = this.ProvinceId3[this.i]-1;
+            break;
+          }
+        }
+      }
+      if (this.Hospital.RegionID == 4) {
+        for (this.i = 0; this.i < this.n4; this.i++) {
+          if (this.Province4[this.i] == this.Hospital.ProvinceID) {
+            this.Hospital.ProvinceID = this.ProvinceId4[this.i]-1;
+            break;
+          }
+        }
+      }
+    },
+    cancel() {
+      window.location.reload();
+    },
     save() {
       console.log("Save");
+      this.convert();
+      console.log(this.Hospital.ProvinceID);
+      if (
+        this.HospitalName == "" ||
+        this.LocationDetails == "" ||
+        this.TelephoneNumber == ""
+      ) {
+        this.dialog = true;
+        return;
+      }
+
       http
         .post(
           "AddHospital/" +
@@ -213,11 +339,12 @@ export default {
         )
         .then(responses => {
           console.log(responses);
-
+          this.status = true;
           this.dialog = true;
         })
         .catch(e => {
           console.log(e);
+          this.dialog = true;
         });
     }
   },
