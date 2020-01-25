a0eaebea223535c692eb2cbd66588b23e94e8401
RakSR <sirilukrongkratok@gmail.com>
2020-01-25 18:25:58 +0700

    close #151 -แก้ไข Home admin เพิ่ม menu ระบบย่อย ระบบจัดโปรโมชั่น

--- frontend/src/components/HomeAdmin.vue
+++ frontend/src/components/HomeAdmin.vue
@@ -37,6 +37,12 @@
               </v-col>
               <v-list-item-title>PayInsurance</v-list-item-title>
             </v-list-item>
+             <v-list-item @click="$router.push('/Promotion')">
+              <v-col class="8">
+                <v-icon>mdi-message-text</v-icon>
+              </v-col>
+              <v-list-item-title>Promotion</v-list-item-title>
+            </v-list-item>
             <v-list-item @click="$router.push('/RenewInsurance')">
               <v-col class="8">
                 <v-icon>event</v-icon>
