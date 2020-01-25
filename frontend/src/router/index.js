89e899f5f7bb4397e4aefc5449db78f83ca4a948
GlomGlom <apichate.yatra@gmail.com>
2020-01-26 05:32:41 +0700

    close #165 - แก้ไข UI ของระบบย่อย ระบบยกเลิกสิทธิ์กรมธรรม์

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,103 +1,114 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-import MainLogin from '../components/MainLogin.vue' 
-import UserLogin from '../components/UserLogin.vue'
-import HomeAdmin from "../components/HomeAdmin.vue"
-import SingInAdmin from "../components/SingInAdmin.vue"
+import MainLogin from "../components/MainLogin.vue";
+import UserLogin from "../components/UserLogin.vue";
+import HomeAdmin from "../components/HomeAdmin.vue";
+import SingInAdmin from "../components/SingInAdmin.vue";
 
-import CreateInsurance from '../views/CreateInsurance.vue'
-import Hospital from '../views/Hospital.vue'
-import PayInsurance from '../views/PayInsurance.vue'
-import User from '../views/User.vue'
-import UserSearch from '../views/UserSearch.vue'
+import CreateInsurance from "../views/CreateInsurance.vue";
+import Hospital from "../views/Hospital.vue";
+import RenewInsurance from "../views/RenewInsurance.vue";
+import Promotion from "../views/Promotion.vue";
+import PayInsurance from "../views/PayInsurance.vue";
+import User from "../views/User.vue";
+import UserSearch from "../views/UserSearch.vue";
 
-import Home from '../views/Home.vue'
-import RegisterInsurance from '../views/RegisterInsurance.vue'
-import ChangeRegister from '../views/ChangeRegister.vue'
-import PrivilegeInsurance from '../views/PrivilegeInsurance.vue'
-import CancelPrivilege from '../views/CancelPrivilege.vue'
+import Home from "../views/Home.vue";
+import RegisterInsurance from "../views/RegisterInsurance.vue";
+import ChangeRegister from "../views/ChangeRegister.vue";
+import PrivilegeInsurance from "../views/PrivilegeInsurance.vue";
+import CancelPrivilege from "../views/CancelPrivilege.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'MainLogin',
+    path: "/",
+    name: "MainLogin",
     component: MainLogin
   },
   {
-    path: '/UserLogin',
-    name: 'UserLogin',
+    path: "/UserLogin",
+    name: "UserLogin",
     component: UserLogin
   },
   {
-    path: '/CreateInsurance',
-    name: 'CreateInsurance',
+    path: "/CreateInsurance",
+    name: "CreateInsurance",
     component: CreateInsurance
   },
   {
-    path: '/Hospital',
-    name: 'Hospital',
+    path: "/Hospital",
+    name: "Hospital",
     component: Hospital
   },
   {
-    path: '/PayInsurance',
-    name: 'PayInsurance',
+    path: "/PayInsurance",
+    name: "PayInsurance",
     component: PayInsurance
   },
   {
-    path: '/User',
-    name: 'User',
-    component: User
-  }, 
+    path: "/Promotion",
+    name: "Promotion",
+    component: Promotion
+  },
   {
-    path: '/Home',
-    name: 'Home',
+    path: "/RenewInsurance",
+    name: "RenewInsurance",
+    component: RenewInsurance
+  },
+  {
+    path: "/User",
+    name: "User",
+    component: User
+  },
+  {
+    path: "/Home",
+    name: "Home",
     component: Home
   },
   {
-    path: '/RegisterInsurance',
-    name: 'RegisterInsurance',
+    path: "/RegisterInsurance",
+    name: "RegisterInsurance",
     component: RegisterInsurance
-  }, 
+  },
   {
-    path: '/PrivilegeInsurance',
-    name: 'PrivilegeInsurance',
+    path: "/PrivilegeInsurance",
+    name: "PrivilegeInsurance",
     component: PrivilegeInsurance
   },
   {
-    path: '/HomeAdmin',
-    name: 'HomeAdmin',
+    path: "/HomeAdmin",
+    name: "HomeAdmin",
     component: HomeAdmin
   },
   {
-    path: '/SingInAdmin',
-    name: 'SingInAdmin',
+    path: "/SingInAdmin",
+    name: "SingInAdmin",
     component: SingInAdmin
   },
   {
-    path: '/CancelPrivilege',
-    name: 'CancelPrivilege',
+    path: "/CancelPrivilege",
+    name: "CancelPrivilege",
     component: CancelPrivilege
   },
   {
-    path: '/ChangeRegister',
-    name: 'ChangeRegister',
+    path: "/ChangeRegister",
+    name: "ChangeRegister",
     component: ChangeRegister
   },
   {
-    path: '/UserSearch',
-    name: 'UserSearch',
+    path: "/UserSearch",
+    name: "UserSearch",
     component: UserSearch
-  },
-  
-]
+  }
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
