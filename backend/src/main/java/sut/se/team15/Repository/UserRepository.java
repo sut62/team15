03ea7dd136d97f7051824895b3cd6011720082e2
Jirawan09 <jirawannitmai09@gmail.com>
2020-01-26 00:55:03 +0700

    close #153 - แก้ไข Repository ของระบบย่อย ระบบค้นหาข้อมูลสมาชิกประกันสุขภาพ

--- backend/src/main/java/sut/se/team15/Repository/UserRepository.java
+++ backend/src/main/java/sut/se/team15/Repository/UserRepository.java
@@ -2,10 +2,20 @@ package sut.se.team15.Repository;
 
 import sut.se.team15.Entity.*;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.repository.query.Param;
+import java.util.Collection;
+import java.util.List;
 
 @RepositoryRestResource
 public interface UserRepository extends JpaRepository<User, Long> {
     User findById(long id);
+
+    // @Query( value = "SELECT * FROM where USERID = 1", nativeQuery = true)
+    // Collection<User> findByUser (@Param("userid") String userid);
+
+    List<User> findByUserid(String userid);
+
 }
